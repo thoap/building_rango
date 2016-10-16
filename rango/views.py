4c7a23dff54a4f5c881200382f65203dd379c097
thoap <thomas.apolke@posteo.de>
2016-10-16 23:51:10 +0200

    adds intertwined links between the views

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
 
+
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+    about_link = '<a href="/rango/about/">About</a>'
+
+    return HttpResponse("Rango says hey there partner!" +
+                        " Do you know the 'about' page yet?" +
+                        " If not have a look under: " +
+                        about_link)
+
+
+def about(request):
+    index_link = '<a href="/rango/">Main page</a>'
+
+    return HttpResponse("Rango says here is the about page. </br>" +
+                        index_link)
