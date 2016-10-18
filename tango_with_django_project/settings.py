b2517555ae8df2b54d9b532fc261feab122355ec
thoap <thomas.apolke@posteo.de>
2016-10-18 23:48:17 +0200

    adds a static files dir and an image to the index html

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,6 +18,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # CREATING A PATH TO OUR TEMPLATE DIRECTORY
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
+# CREATING A PATH TO IMAGES
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -121,5 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
